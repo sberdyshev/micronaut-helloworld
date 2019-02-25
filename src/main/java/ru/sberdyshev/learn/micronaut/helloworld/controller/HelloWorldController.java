4230010a0c7daf31fb2dea7a361f1968ad2f560c
Sergey Berdyshev <sberdyshev@cinimex.ru>
2019-02-25 16:19:31 +0300

    xml\json headers

--- src/main/java/ru/sberdyshev/learn/micronaut/helloworld/controller/HelloWorldController.java
+++ src/main/java/ru/sberdyshev/learn/micronaut/helloworld/controller/HelloWorldController.java
@@ -1,14 +1,29 @@
 package ru.sberdyshev.learn.micronaut.helloworld.controller;
 
-
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
 @Controller("/hello")
 public class HelloWorldController {
-    @Get(produces = MediaType.APPLICATION_JSON)
-    public String index() {
-        return "Hello World";
+    @Get("/")
+    public HttpResponse<?> index(final HttpHeaders headers) {
+        final String message = "Hello World";
+        if (headerAcceptXml(headers)) {
+            final String xml = encodeAsXml(message);
+            return HttpResponse.ok(xml)
+                    .contentType(MediaType.APPLICATION_XML_TYPE);
+        }
+        return HttpResponse.ok(message).contentType(MediaType.APPLICATION_JSON);
+    }
+
+    private boolean headerAcceptXml(final HttpHeaders headers) {
+        return headers.accept().contains(MediaType.APPLICATION_XML_TYPE);
+    }
+
+    private String encodeAsXml(final String message) {
+        return String.format("<content>%s</content>", message);
     }
 }
